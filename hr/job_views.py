b5099642ef1c5d8b74bf3cdb85f5c69474102547
srikanthpragada <srikanthpragada@gmail.com>
2018-11-08 20:58:54 +0530

    08-NOV

--- hr/job_views.py
+++ hr/job_views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import sqlite3
 
 
-def list(request):
+def list_jobs(request):
     # connect to db and retrieve details
     con = sqlite3.connect(r"e:\classroom\python\oct1\hr.db")
     cur = con.cursor()
@@ -13,9 +13,36 @@ def list(request):
     return render(request, 'jobs/list.html', {'jobs': jobs})
 
 
-def add(request):
+def add_job(request):
+    message = ""
+    jobid =""
+    jobtitle = ""
+    minsal = ""
+    if request.method == 'POST':
+        jobid = request.POST["jobid"]
+        jobtitle = request.POST["jobtitle"]
+        minsal = request.POST["minsal"]
+        con = sqlite3.connect(r"e:\classroom\python\oct1\hr.db")
+        cur = con.cursor()
+        try:
+            cur.execute("insert into jobs values(?,?,?)", (jobid, jobtitle, minsal))
+            con.commit()
+            message = "Job has been added successfully!"
+        except:
+            message = "Sorry! Could not add job. Please try again!"
+        finally:
+            con.close()
+
+    # Common for GET and POST
+    return render(request, 'jobs/add.html',
+                  {'message': message,
+                   'jobid' : jobid,
+                   'jobtitle' : jobtitle,
+                   'minsal' : minsal })
+
+
+def search_jobs(request):
     pass
 
-
-def search(request):
+def delete_job(request):
     pass
--- hr/urls.py
+++ hr/urls.py
@@ -6,5 +6,7 @@ from . import views, job_views
 urlpatterns = [
     path('welcome/', views.welcome),
     path('about/', views.about),
-    path('jobs/list', job_views.list),
+    path('jobs/list', job_views.list_jobs),
+    path('jobs/add', job_views.add_job),
+    path('jobs/delete', job_views.delete_job),
 ]
