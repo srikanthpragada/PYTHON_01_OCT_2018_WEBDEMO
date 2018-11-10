72d28f803c2dd05e4f1731cad1a5fd7dae43c664
srikanthpragada <srikanthpragada@gmail.com>
2018-11-10 20:59:55 +0530

    10-NOV

--- hr/job_views.py
+++ hr/job_views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import AddJobForm
 import sqlite3
+
+
+def insert_job(id, title, minsal):
+    con = sqlite3.connect(r"e:\classroom\python\oct1\hr.db")
+    cur = con.cursor()
+    try:
+        cur.execute("insert into jobs values(?,?,?)", (id, title, minsal))
+        con.commit()
+        message = "Job has been added successfully!"
+    except:
+        message = "Sorry! Could not add job. Please try again!"
+    finally:
+        con.close()
+
+    return message
+
+
+def get_job(id):
+    con = sqlite3.connect(r"e:\classroom\python\oct1\hr.db")
+    cur = con.cursor()
+    try:
+        cur.execute("select * from jobs where job_id = ?",(id,))
+        job = cur.fetchone()
+        return job
+    except:
+        return None
+    finally:
+        con.close()
+
 
 
 def list_jobs(request):
@@ -15,38 +45,71 @@ def list_jobs(request):
 
 def add_job(request):
     message = ""
-    jobid =""
+    jobid = ""
     jobtitle = ""
     minsal = ""
     if request.method == 'POST':
         jobid = request.POST["jobid"]
         jobtitle = request.POST["jobtitle"]
         minsal = request.POST["minsal"]
-        con = sqlite3.connect(r"e:\classroom\python\oct1\hr.db")
-        cur = con.cursor()
-        try:
-            cur.execute("insert into jobs values(?,?,?)", (jobid, jobtitle, minsal))
-            con.commit()
-            message = "Job has been added successfully!"
-        except:
-            message = "Sorry! Could not add job. Please try again!"
-        finally:
-            con.close()
+        message = insert_job(jobid, jobtitle, minsal)
 
     # Common for GET and POST
     return render(request, 'jobs/add.html',
                   {'message': message,
-                   'jobid' : jobid,
-                   'jobtitle' : jobtitle,
-                   'minsal' : minsal })
+                   'jobid': jobid,
+                   'jobtitle': jobtitle,
+                   'minsal': minsal})
 
 
 def search_jobs(request):
     pass
 
-def delete_job(request,jobid):
+
+def delete_job(request, jobid):
     print("Job id ", jobid)
-    return  render(request, 'jobs/delete.html',
-                   {'message' : f"Job [{jobid}] has been deleted!"})
+    return render(request, 'jobs/delete.html',
+                  {'message': f"Job [{jobid}] has been deleted!"})
     # delete job with given jobid from JOBS table
     # return redirect('/hr/jobs/list')
+
+
+def add_job_form(request):
+    message = ""
+    if request.method == "GET":
+        f = AddJobForm()
+    else:
+        f = AddJobForm(request.POST)  # Bind data from request to form
+        if f.is_valid():
+            jobid = f.cleaned_data["jobid"]
+            jobtitle = f.cleaned_data["jobtitle"]
+            minsal = f.cleaned_data["minsal"]
+            message = insert_job(jobid,jobtitle,minsal)
+            f = AddJobForm()
+
+    # Common for GET and POST
+    return render(request, 'jobs/add_form.html',
+                  {'message': message,
+                   'form': f})
+
+
+def edit_job(request,jobid):
+     message = ""
+     if request.method == "GET":
+        job = get_job(jobid)
+        if not job is None:
+            f = AddJobForm({'jobid' : job[0],  'jobtitle' : job[1],
+                             'minsal' : job[2]})
+        else:
+            return render(request, "jobs/error.html",
+                            {'message' : 'Job Id Not Found!'})
+     else:
+         # post
+         # Create form and bind data from request.POST
+         # Validate and update JOBS table and set message
+         pass
+
+     # Common for GET and POST
+     return render(request, 'jobs/edit.html',
+                      {'message': message,
+                       'form': f})
--- hr/urls.py
+++ hr/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views, job_views
 
-
 urlpatterns = [
     path('welcome/', views.welcome),
     path('about/', views.about),
     path('jobs/list', job_views.list_jobs),
     path('jobs/add', job_views.add_job),
+    path('jobs/edit/<jobid>', job_views.edit_job),
+    path('jobs/add_form', job_views.add_job_form),
     path('jobs/delete/<jobid>/', job_views.delete_job),
-]
+ ]
